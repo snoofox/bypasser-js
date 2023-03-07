79114466bdfdc2037313dfae483e572041206ff3
webdevpawan <ashwinsingh450@hotmail.com>
2023-03-07 14:13:55 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.post('/bypass', (req, res) => {
+    const url = req.body.url;
+    axios.get(url)
+        .then(response => {
+            const $ = cheerio.load(response.data);
+            const script = $('script').text();
+            const pattern = /link:\s+'(https?:\/\/\S+)'/;
+            const result = pattern.exec(script)
+            res.send(result[1])
+        })
+})
+
+app.listen(3000, () => {
+    console.log('Server listening on port 3000');
+});
